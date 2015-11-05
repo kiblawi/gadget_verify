036c84e259d7c20b3f54a44e6e84820d3f836511
Sid Kiblawi <sid.kiblawi@gmail.com>
2015-11-05 15:27:09 -0600

    added admin template

--- wsgi/gadget_verify/gadget_verify/settings.py
+++ wsgi/gadget_verify/gadget_verify/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'gadget_verify.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
